0c0d3750487f77e48e38b7df592de757627ddb63
yye <yye@gausscode.com>
2017-06-09 15:37:41 -0500

    Chapter 2, template

--- app/views.py
+++ app/views.py
@@ -8,3 +8,28 @@ def index():
     return render_template('index.html',
                            title='Home',
                            user=user)
+
+@app.route('/index1')
+def index1():
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index1.html',
+                           title='',
+                           user=user)
+
+@app.route('/index2')
+def index2():
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template("index2.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
